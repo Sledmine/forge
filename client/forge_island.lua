f44dc1ace5aefce3f11b9a68d32d72730b477d06
JerryBrick <ccs81.mr@gmail.com>
2020-09-03 21:09:57 -0500

    Prevent text drawing when game is on menus

--- client/forge_island.lua
+++ client/forge_island.lua
@@ -244,7 +244,8 @@ function onMapLoad()
 end
 
 function OnPreFrame()
-    if (drawTextBuffer) then
+    local gameOnMenus = read_byte(blam.addressList.gameOnMenus) == 0
+    if (drawTextBuffer and not gameOnMenus) then
         draw_text(table.unpack(drawTextBuffer))
     end
 end
