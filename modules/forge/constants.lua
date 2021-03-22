ad14f91d4c1cfa3b0875d966c1a19ff497d86d20
Sled <angelherxd@hotmail.com>
2021-03-22 15:16:54 -0700

    big refactor to gather constants at start time

--- modules/forge/constants.lua
+++ modules/forge/constants.lua
@@ -1,9 +1,12 @@
 ------------------------------------------------------------------------------
 -- Forge Constants
 -- Sledmine
--- Constants values
-------------------------------------------------------------------------------
+-- Constant values for different purposes
+--[[ The idea behind this module is to gather all the data that does not change
+ across runtime, so we can optimize getting data just once at map load time ]] ------------------------------------------------------------------------------
 local core = require "forge.core"
+
+local time = os.clock()
 
 local constants = {}
 
@@ -13,13 +16,13 @@ constants.mouseInputAddress = 0x64C73C
 
 -- Constant Forge values
 constants.requestSeparator = "&"
-constants.maximumBudget = 1024
+constants.maximumObjectsBudget = 1024
 constants.minimumZSpawnPoint = -18.69
 constants.forgeSelectorOffset = 0.33
 constants.forgeSelectorVelocity = 15
-constants.scenerysTagCollectionPath = core.findTag(
-                                          map:gsub("_dev", ""):gsub("_beta", "") .. "_objects",
-                                          tagClasses.tagCollection)
+
+-- Map name should be the base project name, without build env variants
+constants.absoluteMapName = map:gsub("_dev", ""):gsub("_beta", "")
 
 -- Constant UI widget definition values
 constants.maximumSidebarSize = 249
@@ -27,12 +30,12 @@ constants.minimumSidebarSize = 40
 constants.maximumProgressBarSize = 171
 constants.maximumLoadingProgressBarSize = 422
 
-local fontTagPath, fontTagId = core.findTag("blender_pro_medium_12", tagClasses.font)
-constants.hudFontTagId = fontTagId
-
-local projectileTagPath, projectileTagIndex = core.findTag("forge", tagClasses.projectile)
-constants.forgeProjectilePath = projectileTagPath
-constants.forgeProjectileTagIndex = projectileTagIndex
+constants.hudFontTagId = core.findTag("blender_pro_medium_12", tagClasses.font).id
+local forgeProjectile = core.findTag("forge", tagClasses.projectile)
+constants.forgeProjectilePath = forgeProjectile.path
+constants.forgeProjectileTagIndex = forgeProjectile.index
+constants.fragGrenadeProjectileTagIndex = core.findTag("frag", tagClasses.projectile)
+                                              .index
 
 -- Constant Forge requests data
 constants.requests = {
@@ -48,6 +51,8 @@ constants.requests = {
             {"yaw"},
             {"pitch"},
             {"roll"},
+            {"color"},
+            {"teamIndex"},
             {"remoteId", "I4"}
         }
     },
@@ -62,42 +67,31 @@ constants.requests = {
             {"z", "f"},
             {"yaw"},
             {"pitch"},
-            {"roll"}
+            {"roll"},
+            {"color"},
+            {"teamIndex"}
         }
     },
     deleteObject = {
         actionType = "DELETE_FORGE_OBJECT",
         requestType = "#d",
-        requestFormat = {
-            {"requestType"},
-            {"objectId"}
-        }
+        requestFormat = {{"requestType"}, {"objectId"}}
     },
     flushForge = {actionType = "FLUSH_FORGE"},
     loadMapScreen = {
         actionType = "LOAD_MAP_SCREEN",
         requestType = "#lm",
-        requestFormat = {
-            {"requestType"},
-            {"objectCount"},
-            {"mapName"}
-        }
+        requestFormat = {{"requestType"}, {"objectCount"}, {"mapName"}}
     },
     setMapAuthor = {
         actionType = "SET_MAP_AUTHOR",
         requestType = "#ma",
-        requestFormat = {
-            {"requestType"},
-            {"mapAuthor"}
-        }
+        requestFormat = {{"requestType"}, {"mapAuthor"}}
     },
     setMapDescription = {
         actionType = "SET_MAP_DESCRIPTION",
         requestType = "#md",
-        requestFormat = {
-            {"requestType"},
-            {"mapDescription"}
-        }
+        requestFormat = {{"requestType"}, {"mapDescription"}}
     },
     loadVoteMapScreen = {
         actionType = "LOAD_VOTE_MAP_SCREEN",
@@ -107,20 +101,12 @@ constants.requests = {
     appendVoteMap = {
         actionType = "APPEND_MAP_VOTE",
         requestType = "#av",
-        requestFormat = {
-            {"requestType"},
-            {"name"},
-            {"gametype"},
-            {"mapIndex"}
-        }
+        requestFormat = {{"requestType"}, {"name"}, {"gametype"}, {"mapIndex"}}
     },
     sendMapVote = {
         actionType = "SEND_MAP_VOTE",
         requestType = "#v",
-        requestFormat = {
-            {"requestType"},
-            {"mapVoted"}
-        }
+        requestFormat = {{"requestType"}, {"mapVoted"}}
     },
     sendTotalMapVotes = {
         actionType = "SEND_TOTAL_MAP_VOTES",
@@ -136,52 +122,115 @@ constants.requests = {
     flushVotes = {actionType = "FLUSH_VOTES"}
 }
 
--- Biped tag definitions
+-- Tag Collections ID
+constants.tagCollections = {
+    forgeObjectsTagId = core.findTag(constants.absoluteMapName .. "_objects",
+                                     tagClasses.tagCollection).id
+}
+
+-- Biped Tags ID
 constants.bipeds = {
-    monitor = core.findTag("monitor", tagClasses.biped),
-    spartan = core.findTag("cyborg_mp", tagClasses.biped)
+    monitorTagId = core.findTag("monitor", tagClasses.biped).id,
+    spartanTagId = core.findTag("multibipeds", tagClasses.biped).id
 }
 
--- Weapon hud tag definitions
+-- Weapon HUD Interface Tags ID
 constants.weaponHudInterfaces = {
-    forgeCrosshair = core.findTag("ui\\hud\\forge", tagClasses.weaponHudInterface),
-    forgeWeaponCrosshair = core.findTag("monitor\\forge", tagClasses.weaponHudInterface)
+    forgeCrosshairTagId = core.findTag("ui\\hud\\forge", tagClasses.weaponHudInterface).id
 }
 
+-- Bitmap Tags ID
 constants.bitmaps = {
-    mapLoading0 = core.findTag("forge_loading_progress0", tagClasses.bitmap),
-    mapLoading1 = core.findTag("forge_loading_progress1", tagClasses.bitmap),
-    hudWeapons = core.findTag("hud_msg_icons_full", tagClasses.bitmap)
+    forgingIconFrame0TagId = core.findTag("forge_loading_progress0", tagClasses.bitmap).id,
+    forgeIconFrame1TagId = core.findTag("forge_loading_progress1", tagClasses.bitmap).id
 }
 
 -- UI widget definitions
 constants.uiWidgetDefinitions = {
-    -- forgeMenu = "[shm]\\halo_4\\ui\\shell\\forge_menu\\forge_menu",
     forgeMenu = core.findTag("forge_menu", tagClasses.uiWidgetDefinition),
-    voteMenu = "[shm]\\halo_4\\ui\\shell\\map_vote_menu\\map_vote_menu",
-    voteMenuList = "[shm]\\halo_4\\ui\\shell\\map_vote_menu\\map_vote_menu_list\\vote_menu_list",
-    objectsList = "[shm]\\halo_4\\ui\\shell\\forge_menu\\category_menu\\category_list",
-    amountBar = "[shm]\\halo_4\\ui\\shell\\forge_menu\\budget_dialog\\budget_progress_bar",
-    loadingMenu = "[shm]\\halo_4\\ui\\shell\\loading_menu\\loading_menu",
-    loadingAnimation = "[shm]\\halo_4\\ui\\shell\\loading_menu\\loading_menu_progress_animation",
-    loadingProgress = "[shm]\\halo_4\\ui\\shell\\loading_menu\\loading_progress_bar",
-    loadoutMenu = "[shm]\\halo_4\\ui\\shell\\loadout_menu\\loadout_menu_no_background",
-    mapsList = "[shm]\\halo_4\\ui\\shell\\pause_game\\forge_options_menu\\maps_list\\maps_list",
-    sidebar = "[shm]\\halo_4\\ui\\shell\\pause_game\\forge_options_menu\\forge_map_list_sidebar_bar"
+    voteMenu = core.findTag("map_vote_menu", tagClasses.uiWidgetDefinition),
+    voteMenuList = core.findTag("vote_menu_list", tagClasses.uiWidgetDefinition),
+    objectsList = core.findTag("category_list", tagClasses.uiWidgetDefinition),
+    amountBar = core.findTag("budget_progress_bar", tagClasses.uiWidgetDefinition),
+    loadingMenu = core.findTag("loading_menu", tagClasses.uiWidgetDefinition),
+    loadingAnimation = core.findTag("loading_menu_progress_animation",
+                                    tagClasses.uiWidgetDefinition),
+    loadingProgress = core.findTag("loading_progress_bar", tagClasses.uiWidgetDefinition),
+    -- // TODO An implementation of this should be possible on the future
+    -- loadoutMenu = "[shm]\\halo_4\\ui\\shell\\loadout_menu\\loadout_menu_no_background",
+    mapsList = core.findTag("maps_list", tagClasses.uiWidgetDefinition),
+    sidebar = core.findTag("forge_map_list_sidebar_bar", tagClasses.uiWidgetDefinition)
 }
-
+-- // TODO Migrate all the references to these strings, ensure the correct properties are being used
 -- Unicode string definitions
 constants.unicodeStrings = {
-    budgetCount = "[shm]\\halo_4\\ui\\shell\\forge_menu\\strings\\budget_count",
-    forgeMenuElements = "[shm]\\halo_4\\ui\\shell\\forge_menu\\strings\\elements_text",
-    votingMapsList = "[shm]\\halo_4\\ui\\shell\\map_vote_menu\\strings\\vote_maps_names",
-    votingCountList = "[shm]\\halo_4\\ui\\shell\\map_vote_menu\\strings\\vote_maps_count",
-    pagination = "[shm]\\halo_4\\ui\\shell\\forge_menu\\strings\\pagination",
-    mapsList = "[shm]\\halo_4\\ui\\shell\\pause_game\\strings\\maps_name",
-    pauseGameStrings = "[shm]\\halo_4\\ui\\shell\\pause_game\\strings\\titles_and_headers"
+    budgetCountTagId = core.findTag("budget_count", tagClasses.unicodeStringList).id,
+    forgeMenuElementsTagId = core.findTag("elements_text", tagClasses.unicodeStringList)
+        .id,
+    votingMapsListTagId = core.findTag("vote_maps_names", tagClasses.unicodeStringList).id,
+    votingCountListTagId = core.findTag("vote_maps_count", tagClasses.unicodeStringList)
+        .id,
+    paginationTagId = core.findTag("pagination", tagClasses.unicodeStringList).id,
+    mapsListTagId = core.findTag("maps_name", tagClasses.unicodeStringList).id,
+    pauseGameStringsTagId = core.findTag("titles_and_headers",
+                                         tagClasses.unicodeStringList).id
 }
 
-local monitorTag = blam.getTag(constants.bipeds.monitor, tagClasses.biped)
-constants.monitorTagId = monitorTag.id
+--[[local swordProjectileTagPath, swordProjectileTagIndex =
+    core.findTag("slash", tagClasses.projectile)
+constants.swordProjectileTagIndex = swordProjectileTagIndex]]
+
+constants.colors = {
+    white = "#FFFFFF",
+    black = "#000000",
+    red = "#FE0000",
+    blue = "#0201E3",
+    gray = "#707E71",
+    yellow = "#FFFF01",
+    green = "#00FF01",
+    pink = "#FF56B9",
+    purple = "#AB10F4",
+    cyan = "#01FFFF",
+    cobalt = "#6493ED",
+    orange = "#FF7F00",
+    teal = "#1ECC91",
+    sage = "#006401",
+    brown = "#603814",
+    tan = "#C69C6C",
+    maroon = "#9D0B0E",
+    salmon = "#F5999E"
+}
+
+constants.colorsNumber = {
+    constants.colors.white,
+    constants.colors.black,
+    constants.colors.red,
+    constants.colors.blue,
+    constants.colors.gray,
+    constants.colors.yellow,
+    constants.colors.green,
+    constants.colors.pink,
+    constants.colors.purple,
+    constants.colors.cyan,
+    constants.colors.cobalt,
+    constants.colors.orange,
+    constants.colors.teal,
+    constants.colors.sage,
+    constants.colors.brown,
+    constants.colors.tan,
+    constants.colors.maroon,
+    constants.colors.salmon
+}
+
+-- Name to search in some tags that are ignored at hidding objects as spartan
+constants.hideObjectsExceptions = {"stand", "teleporters"}
+
+constants.teleportersChannels = {
+    alpha = 0,
+    bravo = 1,
+    charly = 2
+}
+
+dprint(string.format("Constants gathered, elapsed time: %.6f\n", os.clock() - time))
 
 return constants
