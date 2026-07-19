a5649a1a2e43c6871c1c8fa42637b44dd08c1c5d
Sledmine <40512223+Sledmine@users.noreply.github.com>
2026-07-18 19:33:29 -0600

    refactor: move player control logic to forge module and streamline main loop

--- lua/modules/forge/main.lua
+++ lua/modules/forge/main.lua
@@ -5,10 +5,8 @@ local getPlayer = engine.gameState.getPlayer
 local getObject = engine.gameState.getObject
 local objectType = engine.tag.objectType
 
-local constants = require "forge.constants"
-local core = require "forge.core"
 local forge = require "forge.forge"
-local bipeds = constants.bipeds
+
 
 local map = {}
 
@@ -19,61 +17,9 @@ function map.main()
 end
 script.startup(map.main)
 
-function map.forgeControl()
-    for playerIndex = 0, 15 do
-        -- Does this player exists?
-        if getPlayer(playerIndex) then
-            -- We create individual anonymous scripts for each player so that each thread
-            -- can sleep independently of other players
-            script.create(function()
-                local player = getPlayer(playerIndex)
-                if not player then
-                    return
-                end
-                local playerBiped = getObject(player.objectHandle.value, objectType.biped)
-                if not playerBiped then
-                    return
-                end
-                local previousPosition = {
-                    x = playerBiped.position.x,
-                    y = playerBiped.position.y,
-                    z = playerBiped.position.z
-                }
-                if playerBiped.unitControlFlags.light and forge.mode == "edit" then
-                    if playerBiped.tagHandle.value == bipeds.spartan.handle.value then
-                        logger:debug("Player {} is pressing light", playerIndex)
-                        core.swapBiped(playerIndex, bipeds.monitor.handle.value)
-                        engine.gameState.deleteObject(player.objectHandle)
-                        sleep(1)
-                        sleep(function()
-                            playerBiped = core.getPlayerObject(playerIndex)
-                            return playerBiped ~= nil
-                        end)
-                        core.teleportPlayer(playerIndex, previousPosition.x, previousPosition.y,
-                                            previousPosition.z)
-                        playerBiped.vitals.health = 1
-                        playerBiped.vitals.shield = 1
-                        -- TODO Restore biped rotation as well
-                        forge.setMonitorMode("idle")
-                    end
-                elseif playerBiped.unitControlFlags.crouch then
-                    logger:debug("Player {} is pressing crouch", playerIndex)
-                    if playerBiped.tagHandle.value == bipeds.monitor.handle.value then
-                        core.swapBiped(playerIndex, bipeds.spartan.handle.value)
-                        engine.gameState.deleteObject(player.objectHandle)
-                        sleep(1)
-                        sleep(function()
-                            return core.getPlayerObject(playerIndex) ~= nil
-                        end)
-                        core.teleportPlayer(playerIndex, previousPosition.x, previousPosition.y,
-                                            previousPosition.z)
-                        forge.setMonitorMode("hidden")
-                    end
-                end
-            end)
-        end
-    end
+function map.loop()
+    forge.controls()
 end
-script.continuous(map.forgeControl)
+script.continuous(map.loop)
 
 return map
